3a1d784086077e221265912b5967bf82ed113861
lozo6 <lorenzo.francisco@stonybrook.edu>
2022-08-28 00:30:53 -0400

    experimentation

--- data-ingestion.py
+++ data-ingestion.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 import requests
 from google.cloud import bigquery
 import xlrd
@@ -8,18 +9,23 @@ import xlrd
 xls = xlrd.open_workbook("C:\\Users\\loren\\hha-data-ingestion\\data\\big3stocks.xls", on_demand=True)
 # finds names of all sheets in excel file
 sheet_names = xls.sheet_names()
+print ('These are the names of all sheets in excel file')
 print(sheet_names)
 tab1 = pd.read_excel('~\\hha-data-ingestion\\data\\big3stocks.xls', sheet_name='AAPL')
 tab2 = pd.read_excel('~\\hha-data-ingestion\\data\\big3stocks.xls', sheet_name='AMZN')
 # prints tab1, tab2
+print('These are the dataframes from excel file in data directory')
 print(tab1, '\n', tab2)
 
 # Section 2
 #using request module for json api
-cms = requests.get('https://data.cms.gov/data-api/v1/dataset/647e8a75-2135-49a0-83a8-f3b12f687d18/data')
+cms = requests.get('https://data.cms.gov/data-api/v1/dataset/1fcb1016-612d-4605-8296-6b220d20e851/data')
 apiDataset = cms.json()
-# prints data
+print('This is the json api dataset')
 print(apiDataset)
+# tried adding for loop to better format data in terminal but keeps saying the json api dataset is a list not a string
+# for key, value in apiDataset.items():
+#     print(key, ':', value)
 
 # Section 3
 client = bigquery.Client.from_service_account_json("C:\\Users\\loren\\hha-data-ingestion\\lorenzo-507-5029d33fcb82.json") # connects to GCP
@@ -30,4 +36,5 @@ query_covid = client.query("SELECT * FROM `bigquery-public-data.covid19_open_dat
 covid19 = query_covid.result() # waits for results
 bigquery2 = pd.DataFrame(covid19.to_dataframe()) # puts data in a formatted DataFrame
 # prints bigquery1, bigquery2
+print('These are the dataframes collected from bigquery-public-data')
 print(bigquery1, '\n', bigquery2)
